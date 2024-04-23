dbe0538f24d0d2380d7b73c155522203cbfb44e3
Sugoto <sugotobasu1@gmail.com>
2024-04-23 09:50:59 +0530

    Add disk and network monitoring gauges

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 def index():
     cpu_metric = psutil.cpu_percent()
     mem_metric = psutil.virtual_memory().percent
+    disk_metric = psutil.disk_usage('/').percent  # Disk usage
+    net_io_metric = psutil.net_io_counters()  # Network I/O statistics
     Message = None
-    if cpu_metric > 80 or mem_metric > 80:
-        Message = "High CPU or Memory Detected, scale up!!!"
+    if cpu_metric > 80 or mem_metric > 80 or disk_metric > 80:
+        Message = "High CPU, Memory or Disk usage detected! Please check the applications running or consider scaling up."
     return render_template(
-        "index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message
+        "index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, disk_metric=disk_metric, net_io_metric=net_io_metric, message=Message
     )
 
 # Main method
